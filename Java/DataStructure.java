7339c0df69ae0a446df24917c43763cf6626368b
João Paulo R. S <joaopauloit@gmail.com>
2016-11-11 23:57:20 -0200

    Alguma implementações de FIFO

---
+++ Java/DataStructure.java
@@ -0,0 +1,108 @@
+import static java.lang.System.out;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class DataStructure{
+
+	public static void main(String args[]){
+
+		Fifo fifo = new Fifo(10);
+
+		BufferedReader br = null;
+		try{
+			br = new BufferedReader(new InputStreamReader(System.in));
+
+			while(true){
+
+				System.out.println("****FIFO****");
+
+				System.out.println("Informe o que deseja fazer:\n 0 - Sair\n 1- Inserir\n 2- Remover\n 3- Exibir itens da fila\n");
+
+				System.out.print("Digite: ");
+
+        String input = br.readLine();
+
+				switch(input){
+					case "0" :
+
+						System.out.println("Programa encerrado com sucesso!\n");
+
+						System.exit(0);
+
+					break;
+					case "1" :
+
+						if (fifo.full()){
+
+								System.out.println("Fila cheia\n");
+
+						}
+						else {
+
+							System.out.println("\nValor do elemento a ser inserido? ");
+
+							System.out.print("Digite: ");
+
+							String valor = br.readLine();
+
+							fifo.insert(valor);
+
+						}
+
+						System.out.println("Item inserido\n");
+
+					break;
+					case "2" :
+						if (estaVazia(&umaFila)){
+
+							printf("\nFila vazia!!!\n\n");
+
+						}
+						else {
+
+							valor = remover(&umaFila);
+							printf("\n%1f removido com sucesso\n\n", valor) ;
+
+						}
+						System.out.println("Item removido\n");
+					break;
+					case "3" :
+
+						String[] itens = fifo.GetItens();
+
+						System.out.println("Itens:\n");
+
+						for (int i=0; i< itens.length; i++){
+
+							if (itens[i] == null || itens[i] == "")
+								break;
+
+							System.out.println((i+1) + " - " + itens[i] + "\n");
+
+						}
+
+						System.out.println("\n\n");
+					break;
+					default:
+						System.out.println("Informe uma opção válida\n");
+					break;
+				}
+				System.out.println("************");
+			}
+		}catch (IOException e) {
+		    e.printStackTrace();
+		} finally {
+		    if (br != null) {
+		        try {
+		            br.close();
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		    }
+		}
+
+	}
+
+}
---
+++ Java/Fifo.java
@@ -0,0 +1,52 @@
+import java.*;
+
+public class Fifo {
+
+	public int capacity;
+	public String[] collection;
+	public int first;
+	public int last;
+	public int numberItens;
+
+	public Fifo(int capacity){
+
+		this.capacity = capacity;
+		this.collection = new String[this.capacity];
+		this.first = 0;
+		this.last = -1;
+		this.numberItens = 0;
+
+	}
+
+	public boolean full(){
+
+		return this.numberItens == this.capacity;
+
+	}
+
+  public boolean free(){
+
+		return this.numberItens == 0;
+
+	}
+
+	public void insert(String value){
+
+		if(this.last == this.capacity-1)
+			this.last = -1;
+
+		this.last++;
+		this.collection[last] = value; // incrementa ultimo e insere
+		this.numberItens++; // mais um item inserido
+
+	}
+
+	public void remove(){
+
+	}
+
+	public String[] GetItens(){
+		return this.collection;
+	}
+
+};
